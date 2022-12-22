942a3407cfcece7322c958f99ba8c116fab506e2
ClansPulp <121213510+ClansPulp@users.noreply.github.com>
2022-12-22 20:51:27 +1100

    Add install script via upload

---
+++ adguard_home/adhuard_home_setup.sh
@@ -0,0 +1,19 @@
+sudo apt update
+sudo apt install curl -y
+sudo apt install docker.io -y
+docker pull adguard/adguardhome
+sudo docker run --name adguardhome\
+    --restart unless-stopped\
+    -v /my/own/workdir:/opt/adguardhome/work\
+    -v /my/own/confdir:/opt/adguardhome/conf\
+    -p 53:53/tcp -p 53:53/udp\
+    -p 67:67/udp -p 68:68/udp\
+    -p 80:80/tcp -p 443:443/tcp -p 443:443/udp -p 3000:3000/tcp\
+    -p 853:853/tcp\
+    -p 784:784/udp -p 853:853/udp -p 8853:8853/udp\
+    -p 5443:5443/tcp -p 5443:5443/udp\
+    -d adguard/adguardhome
+echo 'Installation complete'
+hostname -i
+echo 'Enter the above IP address in a web browser with :80 at the end to access the GUI'
+echo 'i.e. 127.0.1.1:80'
